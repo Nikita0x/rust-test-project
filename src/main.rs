5ca7aa342669d4865ae49d63188b60fae4f69511
Nikita0x <klirmio21@gmail.com>
2026-05-31 10:31:03 +0300

    separated draw and update functions; added hover and centered text for close button

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ struct Window {
 
     title: String,
 
+    button_width: f32,
+    button_height: f32,
+
     is_dragging: bool,
     drag_offset_x: f32,
     drag_offset_y: f32,
@@ -22,7 +25,27 @@ impl Window {
 
         draw_rectangle(self.x, self.y, self.width, self.height, PURPLE); //window
         draw_rectangle(self.x, self.y, self.width, 40.0, BLUE); //titlebar
-        draw_rectangle(self.x + self.width - 40.0, self.y, 40.0, 40.0, RED); //close button
+
+        let close_button_color = if self.is_mouse_over_close_button() {
+            MAGENTA
+        } else {
+            RED
+        };
+
+        draw_rectangle(
+            self.x + self.width - self.button_width,
+            self.y,
+            self.button_width,
+            self.button_height,
+            close_button_color,
+        ); //close button
+
+        let button_x = self.x + self.width - self.button_width;
+        let button_y = self.y;
+
+        let button_center_x = button_x + self.button_width / 2.0;
+        let button_center_y = button_y + self.button_height / 2.0;
+
         draw_text(
             self.title.clone(),
             self.x + 20.0,
@@ -31,6 +54,18 @@ impl Window {
             BLACK,
         );
 
+        let close_button_text_dimensions = measure_text("X", None, 30, 1.0);
+
+        draw_text(
+            "X",
+            button_center_x - close_button_text_dimensions.width / 2.0,
+            button_center_y + close_button_text_dimensions.height / 2.0,
+            30.0,
+            BLACK,
+        );
+    }
+
+    fn update(&mut self) {
         let hovering = self.is_mouse_over_titlebar();
 
         if hovering && is_mouse_button_pressed(MouseButton::Left) {
@@ -65,21 +100,19 @@ impl Window {
         mouse_x >= self.x
             && mouse_x <= self.x + self.width
             && mouse_y >= self.y
-            && mouse_y <= self.y + 40.0
+            && mouse_y <= self.y + self.button_height
     }
 
     fn is_mouse_over_close_button(&self) -> bool {
         let (mouse_x, mouse_y) = mouse_position();
 
-        let button_x = self.x + self.width - 40.0;
+        let button_x = self.x + self.width - self.button_width;
         let button_y = self.y;
-        let button_width = 40.0;
-        let button_height = 40.0;
 
         mouse_x >= button_x
-            && mouse_x <= button_x + button_width
+            && mouse_x <= button_x + self.button_width
             && mouse_y >= button_y
-            && mouse_y <= button_y + button_height
+            && mouse_y <= button_y + self.button_height
     }
 }
 
@@ -95,6 +128,8 @@ async fn main() {
         drag_offset_x: 0.0,
         drag_offset_y: 0.0,
         is_closed: false,
+        button_height: 40.0,
+        button_width: 40.0,
     };
 
     loop {
@@ -110,6 +145,7 @@ async fn main() {
         draw_text(format!("X: {}, Y: {}", x, y), 90.0, 90.0, 30.0, RED);
         draw_text(screen_width().to_string(), 60.0, 60.0, 30.0, BLUE);
 
+        window.update();
         window.draw();
 
         next_frame().await
