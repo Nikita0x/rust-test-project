93957238baa81c4f093d3534c10253f3f2dbb25f
Nikita0x <klirmio21@gmail.com>
2026-06-01 00:03:17 +0300

    added: expanding and shrinking window

--- src/window.rs
+++ src/window.rs
@@ -5,10 +5,14 @@ use crate::geometry::Rect;
 use crate::ui::Button;
 
 pub struct Window {
+    old_x: f32,
+    old_y: f32,
     pub x: f32,
     pub y: f32,
-    pub width: f32,
 
+    old_width: f32,
+    old_height: f32,
+    pub width: f32,
     pub height: f32,
 
     pub title: String,
@@ -23,6 +27,8 @@ pub struct Window {
     expand_button: Button,
     minimize_button: Button,
 
+    is_expanded: bool,
+
     pub is_closed: bool,
 }
 
@@ -30,10 +36,21 @@ impl Window {
     pub fn new(x: f32, y: f32, width: f32, height: f32, title: String, sound: Sound) -> Self {
         let button_width = 40.0;
         let button_height = 40.0;
+        let is_expanded = false;
+
+        let old_x = x;
+        let old_y = y;
+        let old_height = height;
+        let old_width = width;
 
         Self {
+            old_x,
+            old_y,
             x,
             y,
+            old_height,
+            old_width,
+            is_expanded,
             width,
             height,
             title,
@@ -99,6 +116,44 @@ impl Window {
             self.is_closed = true;
         }
 
+        if self.expand_button.is_clicked() {
+            println!("Expand button clicked.");
+            self.is_expanded = !self.is_expanded;
+
+            if self.is_expanded {
+                println!("Expanding...");
+
+                self.old_x = self.x;
+                self.old_y = self.y;
+                self.old_width = self.width;
+                self.old_height = self.height;
+
+                self.width = screen_width();
+                self.height = screen_height();
+                self.set_window_position(0.0, 0.0);
+            } else {
+                println!("Shrinking....");
+                self.width = self.old_width;
+                self.height = self.old_height;
+                self.set_window_position(self.old_x, self.old_y);
+            }
+
+            self.close_button
+                .set_position(self.x + self.width - 40.0, self.y);
+
+            self.expand_button
+                .set_position(self.x + self.width - 80.0, self.y);
+
+            self.minimize_button
+                .set_position(self.x + self.width - 120.0, self.y);
+        }
+
+        if self.minimize_button.is_clicked() {
+            println!("minimize btn clicked");
+            println!("screen width: {}", screen_width());
+            println!("screen height: {}", screen_height());
+        }
+
         let hovering = self.is_mouse_over_titlebar();
 
         if hovering && is_mouse_button_pressed(MouseButton::Left) {
@@ -137,5 +192,10 @@ impl Window {
         let (mouse_x, mouse_y) = mouse_position();
 
         titlebar.contains(mouse_x, mouse_y)
+    }
+
+    fn set_window_position(&mut self, x: f32, y: f32) {
+        self.x = x;
+        self.y = y;
     }
 }
